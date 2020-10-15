3d7ae9697b5d51d5308ef76cec52ce05d7bb61f6
Artem <marsov78@mail.ru>
2020-10-15 11:44:21 +0300

    change name

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -4,12 +4,27 @@ import './Table.css';
 
 function Table() {
   const [data, setData] = useState([]);// можно использоват redux или useContext
-  const [idDel, setIdDel] = useState(0);
-  const [idUpdete, setIdUpdete] = useState(false);
-  const [chengeUpdete, setChengeUpdete] = useState(0);
-  const [inputEl, setInputEl] = useState({ 0: false, 1: false, 2: false, 3: false });
+  const [idDel, setIdDel] = useState(0);// для передачи id элемерта который нужно удалить
+  const [idUpdete, setIdUpdete] = useState(false);// усанавливает id элемента для поиска в массиве
+  const [chengeUpdete, setChengeUpdete] = useState(0);// принемает id элемента что бы сработал запрос на редактирование
+  const [index, setIndex] = useState(0);// индекс элемента массива
+  const [inputEl, setInputEl] = useState([false, false, false, false, false]);// доступ к редактированию елемента
+  const [changeName, setChangeName] = useState('');// для редактирования имени
   function heandleSave(id) {
     setChengeUpdete(id);
+    setInputEl([false, false, false, false, false]);
+    // setChangeName(changeName);
+  }
+  // редактирование имени
+  function changeChoiceName(event) {
+    setChangeName(event.target.value);
+  }
+  function chahgeInputEl(i) {
+    const newData = inputEl.map((el) => el);
+    newData.splice(i, 1, !inputEl[i]);
+    setInputEl(newData);
+    setIndex(i);
+    setChangeName(data[i][1].value);
   }
   // удаление элемента
   useEffect(() => {
@@ -56,16 +71,19 @@ function Table() {
       // const result = await respons.json();
       // setData(() => result);
       const newEl = data.find((el) => el[0].value === idUpdete);
-      const newData = data.filter((el) => el[0].value !== idUpdete);
-      newData.push(newEl);
+      newEl[1].value = changeName;
+      // const newData = data.filter((el) => el[0].value !== idUpdete);
+      const newData = data.map((el) => el);
+      newData.splice(index, 1, newEl);
+      // newData.push(newEl);
       setData(newData);
     }
     // что бы первый раз не сработал
     if (chengeUpdete) {
       somePut();
       setIdUpdete(false);
+      setChangeName('');
     }
-    
   }, [chengeUpdete]);
   // начальная загрузка
   useEffect(() => {
@@ -94,20 +112,20 @@ function Table() {
           <h2 className="el">act</h2>
         </div>
         {data.map((el, i) => (
-          <div key={el[0].value} className="data">
+          <div key={Math.random()} className="data">
             <h5 className="el1">{el[0].value}</h5>
             {!inputEl[i] && <h5 className="el1">{el[1].value}</h5>}
-            {inputEl[i] && <input type="text" className="el1" />}
+            {inputEl[i] && <input className="el2" onChange={changeChoiceName} value={changeName} />}
             {!inputEl[i] && <h5 className="el1">{el[2].value}</h5>}
-            {inputEl[i] && <input type="text" className="el1" />}
+            {inputEl[i] && <input type="text" className="el2" value={el[2].value} />}
             {!inputEl[i] && <h5 className="el1">{el[3].value}</h5>}
-            {inputEl[i] && <input type="text" className="el1" />}
+            {inputEl[i] && <input type="text" className="el2" value={el[3].value} />}
             {!inputEl[i] && <h5 className="el1">{el[4].value}</h5>}
-            {inputEl[i] && <input type="text" className="el1" />}
+            {inputEl[i] && <input type="text" className="el2" value={el[4].value} />}
             <div className="el1">
               <button onClick={() => setIdDel(el[0].value)}>Del</button>
-              {!idUpdete && <button onClick={() => { setIdUpdete(el[0].value); setInputEl({...inputEl, i:!inputEl[i]}); console.log('inputEl', inputEl);}}>Update</button>}
-              {idUpdete && <button onClick={() => { heandleSave(el[0].value); setInputEl({...inputEl, i:!inputEl[i]})}}>Save</button>}
+              {!inputEl[i] && <button onClick={() => { setIdUpdete(el[0].value); chahgeInputEl(i); }}>Update</button>}
+              {inputEl[i] && <button onClick={() => { heandleSave(el[0].value); }}>Save</button>} {/* idUpdete */}
             </div>
           </div>
         ))}
